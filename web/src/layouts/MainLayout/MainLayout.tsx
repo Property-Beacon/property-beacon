ba60bd3278ff15bbe8e4b858fc45f507af4dffc5
Brian Liu <brian.liu@edapp.com>
2021-05-18 17:39:30 +1000

    First commit

---
+++ web/src/layouts/MainLayout/MainLayout.tsx
@@ -0,0 +1,90 @@
+import { Link, routes } from '@redwoodjs/router'
+
+const MainLayout: React.FunctionComponent = ({ children }) => {
+  return (
+    <div className="grid grid-cols-1 gap-6 p-2 lg:p-6 xl:grid-cols-3 lg:bg-base-200 rounded-box">
+      <div className="navbar col-span-1 shadow-lg xl:col-span-3 bg-neutral-focus text-neutral-content rounded-box">
+        <div className="flex-none">
+          <button className="btn btn-square btn-ghost">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="feather feather-menu"
+            >
+              <line x1="3" y1="12" x2="21" y2="12"></line>
+              <line x1="3" y1="6" x2="21" y2="6"></line>
+              <line x1="3" y1="18" x2="21" y2="18"></line>
+            </svg>
+          </button>
+        </div>
+        <div className="flex-none px-2 mx-2">
+          <span className="text-lg font-bold">Property Beacon</span>
+        </div>
+        <div className="flex justify-center flex-1 px-2 mx-2">
+          <div className="items-stretch hidden lg:flex">
+            <Link
+              to={routes.home()}
+              className="btn btn-ghost btn-sm rounded-btn"
+            >
+              Home
+            </Link>
+            <Link
+              to={routes.about()}
+              className="btn btn-ghost btn-sm rounded-btn"
+            >
+              About
+            </Link>
+          </div>
+        </div>
+        <div className="flex-none">
+          <button className="btn btn-square btn-ghost">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="feather feather-bell"
+            >
+              <path d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9"></path>
+              <path d="M13.73 21a2 2 0 0 1-3.46 0"></path>
+            </svg>
+          </button>
+        </div>
+        <div className="flex-none">
+          <button className="btn btn-square btn-ghost">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="feather feather-search"
+            >
+              <circle cx="11" cy="11" r="8"></circle>
+              <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
+            </svg>
+          </button>
+        </div>
+      </div>
+      <main className="col-span-1 xl:col-span-3">{children}</main>
+    </div>
+  )
+}
+
+export default MainLayout
