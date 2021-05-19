91a8c318298d2857101b5a61cf0cffc5ab8778f3
Brian Liu <brian.liu@edapp.com>
2021-05-19 17:43:52 +1000

    Added og data and variables

---
+++ web/config/webpack.config.js
@@ -0,0 +1,28 @@
+/** @returns {import('webpack').Configuration} Webpack Configuration */
+module.exports = (config, { mode }) => {
+  if (mode === 'development') {
+    // Add dev plugin
+  }
+
+  // Add custom rules for your project
+  // config.module.rules.push(YOUR_RULE)
+
+  // Add custom plugins for your project
+  // config.plugins.push(YOUR_PLUGIN)
+
+  config.plugins.forEach((plugin) => {
+    if (plugin.constructor.name === 'HtmlWebpackPlugin') {
+      plugin.options.title = process.env.npm_package_displayName
+      plugin.options.templateParameters = {
+        ogUrl: process.env.npm_package_homepage,
+        ogTitle: process.env.npm_package_displayName,
+        ogDescription: process.env.npm_package_description,
+        ogImage: '',
+        manifest: '/manifest.webmanifest',
+        ...plugin.options.templateParameters
+      }
+    }
+  })
+
+  return config
+}
